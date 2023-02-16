50a33b43bea73def0b6b2b882fc382357a8ac416
changkong <593943519@qq.com>
2023-02-16 18:00:40 +0800

    add epoll to wait for signal and dealwith it

--- main.c
+++ main.c
@@ -16,6 +16,9 @@
 
 #include <sys/wait.h>
 
+#include <fcntl.h>
+#include <sys/epoll.h>
+
 #include "init.h"
 #include "log.h"
 #include "parser.h"
@@ -26,20 +29,34 @@
 // gdb --tui ./main
 // ddd ./main
 
-void sigchld_handler(int signo)
+static int signal_fd = -1;
+static int signal_recv_fd = -1;
+
+static void sigchld_handler(int signo)
 {
+	NOTICE("%s: signo = %d\n", __func__, signo);
+	
+	write(signal_fd, &signo, sizeof(signo));
+}
+
+static int wait_for_one_process(int block)
+{
+	pid_t pid;
 	int status;
 	struct listnode *node;
     struct command *cmd;
 	struct service *svc;
 	
 	// pid_t pid = wait(&status);
-	pid_t pid = waitpid(-1, &status, WNOHANG);
+	//pid_t pid = waitpid(-1, &status, WNOHANG);
+	while ( (pid = waitpid(-1, &status, block ? 0 : WNOHANG)) == -1 && errno == EINTR );
+    if (pid <= 0) return -1;
+    INFO("waitpid returned pid %d, status = %08x\n", pid, status);
 	
     svc = service_find_by_pid(pid);
     if (!svc) {
         ERROR("untracked pid %d exited\n", pid);
-        return;
+        return 0;
     }
 
     NOTICE("process '%s', pid %d exited\n", svc->name, pid);
@@ -61,7 +78,7 @@ void sigchld_handler(int signo)
     /* disabled and reset processes do not get restarted automatically */
     if (svc->flags & (SVC_DISABLED | SVC_RESET) )  {
         notify_service_state(svc->name, "stopped");
-        return;
+        return 0;
     }
 	
     svc->flags &= (~SVC_RESTART);
@@ -73,6 +90,39 @@ void sigchld_handler(int signo)
         cmd->func(cmd->nargs, cmd->args);
     }
     notify_service_state(svc->name, "restarting");
+	
+	return 0;
+}
+
+#define EPOLL_LISTEN_CNT        256
+#define EPOLL_LISTEN_TIMEOUT    500
+
+void handle_signal(void)
+{
+    char tmp[32];
+
+    /* we got a SIGCHLD - reap and restart as needed */
+    read(signal_recv_fd, tmp, sizeof(tmp));
+    while (!wait_for_one_process(0));
+}
+
+int epoll_add_fd(int epfd, int fd)
+{
+    int ret;
+    struct epoll_event event;
+
+    memset(&event, 0, sizeof(event));
+    event.data.fd = fd;
+    event.events = EPOLLIN | EPOLLET;
+
+    ret = epoll_ctl(epfd, EPOLL_CTL_ADD, fd, &event);
+    if(ret < 0) {
+        ERROR("epoll_ctl Add fd:%d error, Error:[%d:%s]", fd, errno, strerror(errno));
+        return -1;
+    }
+
+    NOTICE("epoll add fd:%d--->%d success", fd, epfd);
+    return 0;    
 }
 
 int main(int argc, char *argv[])
@@ -84,30 +134,59 @@ int main(int argc, char *argv[])
     act.sa_flags = SA_NOCLDSTOP;
     sigaction(SIGCHLD, &act, 0);
 	
-	char value[64];
-	property_get("USER", value);
-	printf("value = %s\n", value);
-	
 	init_parse_config_file("./init.rc");
 	
 	action_for_each_trigger("early-init", action_add_queue_tail);
 	
 	/* execute all the boot actions to get us started */
     action_for_each_trigger("init", action_add_queue_tail);
-    
-	int count = 0;
+	
+	/* create epoll fd */
+    int epfd = epoll_create(EPOLL_LISTEN_CNT); 
+	
+	int s[2];
+	
+    /* create a signalling mechanism for the sigchld handler */
+    if (socketpair(AF_UNIX, SOCK_STREAM, 0, s) == 0) {
+        signal_fd = s[0];
+        signal_recv_fd = s[1];
+        fcntl(s[0], F_SETFD, FD_CLOEXEC);
+        fcntl(s[0], F_SETFL, O_NONBLOCK);
+        fcntl(s[1], F_SETFD, FD_CLOEXEC);
+        fcntl(s[1], F_SETFL, O_NONBLOCK);
+		
+		if (epoll_add_fd(epfd, signal_recv_fd)) {
+			close(signal_recv_fd);
+			return -1;
+		}
+    }
+	handle_signal();
+
+	struct epoll_event events[EPOLL_LISTEN_CNT];    
+    memset(events, 0, sizeof(events));
 
 	for(;;) {
-		execute_one_command();
+		int rc = execute_one_command();
 		
 		restart_processes();
 		
-		//sleep(1);
+		if (rc != 0) {
+			continue;
+		}
 		
-		usleep(5000);	// 50000 微妙
+		/* wait epoll event */
+        int fd_cnt = epoll_wait(epfd, events, EPOLL_LISTEN_CNT, EPOLL_LISTEN_TIMEOUT); // EPOLL_LISTEN_TIMEOUT （millisecond）
 		
-		if (++count > 100) {
-			break;
+		// NOTICE("fd_cnt = %d, epfd = %d\n", fd_cnt, epfd);
+		
+		for(int i = 0; i < fd_cnt; i++)  {
+			if (events[i].events & EPOLLIN)  {
+				if (events[i].data.fd == signal_recv_fd) {
+					NOTICE("fd_cnt = %d, epfd = %d\n", fd_cnt, epfd);
+					
+					handle_signal();
+				}
+			}
 		}
 	}
     
